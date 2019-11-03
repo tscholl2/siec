99015bf62bb1918ffb8281cf8bbb2e0c0ab93580
Travis Scholl <traviswscholl@gmail.com>
2019-11-03 08:12:44 -0500

    restrict to cofactor 1

--- search/main.go
+++ search/main.go
@@ -11,30 +11,31 @@ import (
 func main() {
 	// build results
 	var results [][2]*big.Int
-	M := new(big.Int).Lsh(one, 256)
+	M := new(big.Int).Sub(new(big.Int).Lsh(one, 256), big.NewInt(1))
 	var mutex = &sync.Mutex{}
 	var waitGroup sync.WaitGroup
 	waitGroup.Add(255)
-	for j := 0; j < 255; j++ {
+	for j := -1; j < 10; j++ {
 		go func(j int) {
 			defer waitGroup.Done()
-			Mj := new(big.Int).Lsh(one, uint(j))
-			Mj.Sub(M, Mj)
-			q1, t1 := nextSiec(Mj)
-			q2, t2 := prevSiec(Mj)
-			Mj = new(big.Int).Lsh(one, uint(j))
-			Mj.Sub(M, Mj)
-			q3, t3 := nextSiec(Mj)
-			q4, t4 := prevSiec(Mj)
-			mutex.Lock()
-			results = append(
-				results,
-				[2]*big.Int{q1, t1},
-				[2]*big.Int{q2, t2},
-				[2]*big.Int{q3, t3},
-				[2]*big.Int{q4, t4},
-			)
-			mutex.Unlock()
+			q := new(big.Int).Set(M)
+			if j >= 0 {
+				c := big.NewInt(1)
+				c.Lsh(c, uint(j))
+				q.Sub(q, c)
+			}
+			for {
+				var t *big.Int
+				q, t = prevSiec(q)
+				N := new(big.Int).Add(q, big.NewInt(1))
+				N.Sub(N, t)
+				if N.ProbablyPrime(25) {
+					mutex.Lock()
+					results = append(results, [2]*big.Int{q, t})
+					mutex.Unlock()
+					return
+				}
+			}
 		}(j)
 	}
 	waitGroup.Wait()
@@ -58,7 +59,7 @@ func main() {
 	})
 	// print
 	for _, r := range results {
-		fmt.Println("q = %d\nt = %d", r[0], r[1])
+		fmt.Printf("q = %d\nt = %d\n", r[0], r[1])
 	}
 }
 
